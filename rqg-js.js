8056404ba09d4eb6451bad4eabeab07a26382294
Víctor <vsmvsmseg@gmail.com>
2018-01-16 17:49:12 +0100

    Add files via upload
    Latest

--- rqg-js.js
+++ rqg-js.js
@@ -4,29 +4,41 @@ $(document).ready(function(){
 	
     function getRaQuote(){
 		$.ajax({
+			
+			//Do request from:
 			url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies&count=1",
+			
+			//Api requirements
 			headers: {
 				"X-Mashape-Key" : "kZ1GUaTuxYmsh7fx5pDbMCzxECC4p1rPnObjsnsYPNuylyDlbk",
 			},
+			
+			//If ok:
 			success: function(resp){
 				
+				//convert the response to json format (it comes as a string) 
 				if (typeof resp === 'string') {
 				resp = JSON.parse(resp);
 				}
 				
-				console.log(resp);
+				//console.log(resp);
 				
+				//Put the new quote in the html elements
+				$("#theQuote").html("\""+resp.quote+"\""); 	//we could use $("#theQuote").text(...); 
+				$("#theAuthor").html("-"+resp.author); 		//or $("#theAuthor").text(...);
+				
+				//Give URL format to share it in twitter
 				returnedQuote = encodeURIComponent("\""+resp.quote+"\"");
 				returnedAuthor = encodeURIComponent("-"+resp.author);
 				returnedCategory = encodeURIComponent(resp.category);
 				
-				$("#theQuote").html("\""+resp.quote+"\""); 	//or $("#theQuote").text(); 
-				$("#theAuthor").html("-"+resp.author); 		//or $("#theAuthor").text();
+				//Replace the link with the current(encoded) quote, author, hashtags...
 				$("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + returnedQuote + returnedAuthor + "&hashtags=" + returnedCategory);
 				}
 		});
 	}
 	
+	//Listen to the click event and refresh the quote
 	$('#newRaQuote').on('click', getRaQuote);
 	
 });
